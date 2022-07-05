b15ba28309121f0e84f8bf761b615ec8199bef29
ebere.tech <okeyebereblessing@gmail.com>
2022-07-05 12:04:47 +0100

    jenkinsproject

---
+++ src/main/java/print/selenium_project/Javaprint.java
@@ -0,0 +1,8 @@
+package print.selenium_project;
+
+public class Javaprint {
+	// the hello world program
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/print/selenium_project/javaprint.java
@@ -0,0 +1,14 @@
+package print.selenium_project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class javaprint {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
